abd3556fff2942aa0192e63d121ea5d3d18201f7
Edu <edu_marzo_98@hotmail.com>
2025-02-19 16:45:00 +0000

    Addig plates to shopping cart

--- src/index.js
+++ src/index.js
@@ -2,8 +2,22 @@ const url = "./assets/data/food.json";
 const mainFoodTemplate = document.querySelector("[main-food-template]");
 const mainCardsContainer = document.querySelector("[main-cards-container]");
 const cartContainer = document.querySelector(".shopping-cart-container");
+// const buyButton = document.querySelector("[buy-cart-button]");
+const totalPriceDiv = document.querySelector(".total-price");
 
+let totalPrice;
 const cart = {};
+
+const increaseQuantity = (e) => {
+  const cartItem = e.currentTarget.closest(".cart-card");
+  const plate = cartItem.querySelector(".product-name").textContent;
+  const quantity = cartItem.querySelector(`.quantity-data-${plate}`);
+  console.log(cartItem);
+  quantity.textContent = quantity.textContent * 1 + 1;
+  addToCart(cart);
+  showTotalPrice(getTotalPrice());
+  console.log(quantity);
+};
 
 const addToCart = (cart) => {
   cartContainer.innerHTML = "";
@@ -14,26 +28,49 @@ const addToCart = (cart) => {
       cartItem.classList = "cart-card";
       cartItem.setAttribute("data-plate", selectedMeal[0]);
       cartItem.innerHTML = `
-        <img src="" alt="">
+      <img src="/assets/img/${selectedMeal[0]}.jpg" alt="">
         <div class="product-name">${selectedMeal[0]}</div>
-        <div class="quantity">${selectedMeal[1]}</div>
-        <div class="price"></div>
-      `;
+        <div class="quantity">
+        <button class="plus-botton-${selectedMeal[0]}">➕</button>
+        <span class="quantity-data-${selectedMeal[0]}">${selectedMeal[1].quantity}</span>
+        <button class="minus-botton-${selectedMeal[0]}">➖</button>
+        </div>
+        <div class="price">${selectedMeal[1].price * selectedMeal[1].quantity} €</div>
+        `;
       cartContainer.appendChild(cartItem);
     } else {
       cartItem.querySelector(".quantity").textContent = selectedMeal[1];
     }
+    const plusBotton = cartItem.querySelector(`.plus-botton-${selectedMeal[0]}`);
+    console.log(plusBotton);
+    plusBotton.addEventListener("click", increaseQuantity);
   });
+};
+
+const getTotalPrice = () => {
+  totalPrice = 0;
+  Object.entries(cart).forEach(plate => {
+    totalPrice = totalPrice + (plate[1].quantity * parseFloat(plate[1].price).toFixed(2));
+  });
+  return totalPrice;
+};
+
+const showTotalPrice = (price) => {
+  totalPriceDiv.innerHTML = `
+    <span>${price.toFixed(2)}</span>
+  `;
 };
 
 const savePlate = e => {
   const clickedItem = e.currentTarget;
   const name = clickedItem.querySelector("[main-food-name]").textContent.toLowerCase();
+  const price = clickedItem.querySelector("[main-food-price]").textContent.split(" ")[0];
   if (!cart[name]) {
-    cart[name] = 0;
+    cart[name] = { quantity: 0, price };
   }
-  cart[name]++;
+  cart[name].quantity++;
   addToCart(cart);
+  showTotalPrice(getTotalPrice());
 };
 
 fetch(url)
